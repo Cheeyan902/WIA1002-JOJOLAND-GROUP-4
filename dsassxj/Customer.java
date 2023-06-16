23f2c78104f873981c0e0f7a7ed34a8d1b8997c4
TIEW CHEE YAN <117051981+Cheeyan902@users.noreply.github.com>
2023-06-16 14:26:43 +0800

    Add files via upload

---
+++ dsassxj/Customer.java
@@ -0,0 +1,26 @@
+package dsassxj;
+
+public class Customer {
+    private String name;
+    private String restaurantName;
+    private String order;
+
+    public Customer(String name, String restaurantName, String order) {
+        this.name = name;
+        this.restaurantName = restaurantName;
+        this.order = order;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+    
+    public String getOrder() {
+        return order;
+    }
+    
+}
