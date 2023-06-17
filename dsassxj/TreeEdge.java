41b0afdd845ce1e60e677d542b6253e8ef8ecb7d
TIEW CHEE YAN <117051981+Cheeyan902@users.noreply.github.com>
2023-06-17 12:45:38 +0800

    Add files via upload

---
+++ dsassxj/TreeEdge.java
@@ -0,0 +1,11 @@
+package dsassxj;
+
+public class TreeEdge<T extends Comparable<T>> {
+    TreeNode<T> parentNode;
+    TreeEdge<T> nextEdge;
+    
+    public TreeEdge(TreeNode<T> parentNode, TreeEdge<T> nextEdge){
+        this.parentNode = parentNode;
+        this.nextEdge = nextEdge;
+    }
+}
